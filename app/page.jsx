9588386076e2e4eb1b23e7775585e0c8f957eb53
inkmobilecruzverde-hue <inkmobilecruzverde@gmail.com>
2026-04-30 15:47:41 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,13 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
   const [form, setForm] = useState({
+    const [ordenes, setOrdenes] = useState([]);
+  useEffect(() => {
+  const data = JSON.parse(localStorage.getItem("ordenes")) || [];
+  setOrdenes(data);
+}, []);
     numero: "",
     fecha: "",
     nombre: "",
@@ -24,14 +29,67 @@ export default function Home() {
   };
 
   const guardarOrden = () => {
-    let ordenes = JSON.parse(localStorage.getItem("ordenes")) || [];
-    ordenes.push(form);
-    localStorage.setItem("ordenes", JSON.stringify(ordenes));
-    alert("Orden guardada 🔥");
-  };
+  let nuevas = [...ordenes, form];
+  localStorage.setItem("ordenes", JSON.stringify(nuevas));
+  setOrdenes(nuevas);
+  alert("Orden guardada 🔥");
+};
 
-  return (
+return (
+  <div>
+
+    <h2>Órdenes guardadas</h2>
+
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Nº</th>
+          <th>Nombre</th>
+          <th>Teléfono</th>
+          <th>Dispositivo</th>
+          <th>Estado</th>
+        </tr>
+      </thead>
+      <tbody>
+        {ordenes.map((o, i) => (
+          <tr key={i}>
+            <td>{o.numero}</td>
+            <td>{o.nombre}</td>
+            <td>{o.telefono}</td>
+            <td>{o.dispositivo}</td>
+            <td>{o.estado}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+
     <div>
+      <h1>Nueva Orden</h1>
+
+      <input name="numero" placeholder="Nº Orden" onChange={handleChange} />
+      <input name="fecha" type="date" onChange={handleChange} />
+      <input name="nombre" placeholder="Nombre" onChange={handleChange} />
+      <input name="dni" placeholder="DNI" onChange={handleChange} />
+      <input name="telefono" placeholder="Teléfono" onChange={handleChange} />
+
+      <input name="dispositivo" placeholder="Dispositivo" onChange={handleChange} />
+      <input name="codigo" placeholder="Código bloqueo" onChange={handleChange} />
+      <input name="problema" placeholder="Problema" onChange={handleChange} />
+      <input name="notas" placeholder="Notas" onChange={handleChange} />
+      <input name="presupuesto" placeholder="Presupuesto" onChange={handleChange} />
+
+      <select name="estado" onChange={handleChange}>
+        <option>Recibido</option>
+        <option>Pendiente</option>
+        <option>Pendiente de recambio</option>
+        <option>Finalizado</option>
+      </select>
+
+      <button onClick={guardarOrden}>Guardar orden</button>
+    </div>
+
+  </div>
+);
       <h1>Nueva Orden</h1>
 
       <input name="numero" placeholder="Nº Orden" onChange={handleChange} />
